8d2fb1961aeb1a474717b04d0790eca205d370ef
Francisco <ftassgaj@gmail.com>
2026-05-07 01:10:12 -0400

    [feat]: Input y chat bubbles modificados

--- mc-chatbot/src/components/chat/chat.jsx
+++ mc-chatbot/src/components/chat/chat.jsx
@@ -10,12 +10,13 @@ function Chat() {
     { id: 5, role: "user", text: "Nooo" },
     { id: 6, role: "bot", text: "Lo siento, no te entiendo" },
     { id: 7, role: "user", text: "Mish" },
+    { id: 9, role: "bot", text: "para craftear un pico de madera, necesitas dos palos y 3 bloques de cualquier madera, en al configuracion: {[madera, madera, madera]. [0, palo, 0]. [0, palo, 0]}" },
   ];
 
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
-        <div className="col-md-8">
+        <div className="col-md-8 p-5 my-1">
           <div className="chat_body d-flex flex-column">
             <div className="col-12 d-flex justify-content-center">
               <h2>¡Bienvenido a MC Craftbot!</h2>
@@ -32,9 +33,23 @@ function Chat() {
 
           
             <div className="col-12">
-              <div className="chat_input form-control">
-                <input type="text" />
-              </div>
+              <form className="chat_input">
+                <div className="input_wrapper my-3">
+                  <input type="text" className="form-control chat-form" />
+                  <button type="submit" className="btn send_btn">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="22"
+                      height="22"
+                      fill="currentColor"
+                      viewBox="0 0 16 16"
+                      aria-hidden="true"
+                    >
+                      <path d="M15.854.146a.5.5 0 0 0-.527-.11l-15 6a.5.5 0 0 0 .006.932L5.5 9.25V14.5a.5.5 0 0 0 .832.374l2.57-2.304 3.5 2.25a.5.5 0 0 0 .768-.32l2.5-13a.5.5 0 0 0-.316-.604zM6 8.933l7.724-6.186L6 9.75v-0.817z" />
+                    </svg>
+                  </button>
+                </div>
+              </form>  
             </div>
           </div>
         </div>
